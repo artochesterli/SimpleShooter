e4620d000359d10572472b14ca800372810679bc
Chang <cl4929@nyu.edu>
2020-08-19 19:19:29 -0400

    Finish character movement and camera

---
+++ SimpleShooter/Intermediate/Build/Win64/SimpleShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "G:/unreal projects/SimpleShooter/SimpleShooter/Intermediate/Build/Win64/SimpleShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ SimpleShooter/Source/SimpleShooter/ShooterCharacter.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterCharacter.h"
+
+// Sets default values
+AShooterCharacter::AShooterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AShooterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AShooterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis(TEXT("LookUpRate"), this, &AShooterCharacter::LookUpRate);
+	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis(TEXT("LookRightRate"), this, &AShooterCharacter::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Released, this, &ACharacter::StopJumping);
+
+}
+
+void AShooterCharacter::MoveForward(float Axis) 
+{
+	AddMovementInput(GetActorForwardVector() * Axis);
+}
+
+void AShooterCharacter::MoveRight(float Axis)
+{
+	AddMovementInput(GetActorRightVector() * Axis);
+}
+
+void AShooterCharacter::LookUpRate(float Axis) 
+{
+	AddControllerPitchInput(Axis*RotationRate*GetWorld()->GetDeltaSeconds());
+}
+
+void AShooterCharacter::LookRightRate(float Axis)
+{
+	AddControllerYawInput(Axis*RotationRate*GetWorld()->GetDeltaSeconds());
+}
+
+
+
